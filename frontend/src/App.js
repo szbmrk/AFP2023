e303762d041e1a14e054d0d31f13166ca576ce38
szbmrk <szmark16@gmail.com>
2023-09-21 12:34:49 +0200

    alap navigálás login / register között
    Co-Authored-By: Réka Erdész <125026056+frake92@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,29 @@
+// Layout.js
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Login from './components//Login';
+import Register from './components/Register';
+
 function App() {
     return (
-        <h1>
-            Hello World
-        </h1>
+        <BrowserRouter>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/register">Register</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path='/login' Component={Login} />
+                    <Route path='/register' Component={Register} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
